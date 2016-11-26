4aebe08e690b2fd849f7729ad21345f2b6a9c983
root <root@localhost.localdomain>
2016-11-26 00:19:31 -0800

    script that show how comments work

---
+++ comments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script is to show how commments works
+
+clear  # clears the screen
+
+MYUSERNAME="Narendra" # the username for this application
+
+echo "We are using the default user called :$MYUSERNAME " # display to console
+
+DATETIMESTAMP=`date`
+
+echo "This is when the script run $DATETIMESTAMP"
+
