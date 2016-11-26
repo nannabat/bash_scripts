a10ea52c4352c75dd58c04436c6f11e27184786c
Narendra Annabathula <narendra.33@gmail.com>
2016-11-25 23:26:59 -0800

    script just environment variables

---
+++ env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#This script will just script some environment variable on to std out
+
+clear
+
+echo "This script will give us environment information"
+
+echo "=============================================================="
+echo " "
+echo "Hello username: $USER"
+echo "your home directory is:$HOME"
+echo ""
+echo "your history file will ignore $HISTCONTROL"
+echo ""
+echo "your terminal session type is:$TERM"
